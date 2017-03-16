8922797088657d6a56a1c9c97c846ba137dc1408
Alexey Boltava <goodmoveab@gmail.com>
2017-03-16 21:51:53 +0700

    Fix interface to conform to filter immutability

--- lab1/src/ru/nsu/ccfit/boltava/filter/IFilter.java
+++ lab1/src/ru/nsu/ccfit/boltava/filter/IFilter.java
@@ -1,12 +1,8 @@
 package ru.nsu.ccfit.boltava.filter;
 
-import ru.nsu.ccfit.boltava.filter.composite.CompositeFilter;
-
+import java.io.IOException;
 import java.nio.file.Path;
 
 public interface IFilter {
-    boolean check(Path fileName) throws IllegalAccessException;
-    default void add(IFilter filter) throws IllegalAccessException {
-        throw new IllegalAccessException("Cannot add elements to primitive structures");
-    };
+    boolean check(Path fileName) throws IllegalAccessException, IOException;
 }
