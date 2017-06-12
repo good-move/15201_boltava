7f30ee75981ac635273fff5d2e6639354c9d0f94
Alexey Boltava <goodmoveab@gmail.com>
2017-06-13 01:13:51 +0700

    Create basic Server

--- lab3/src/ru/nsu/ccfit/boltava/model/Server.java
+++ lab3/src/ru/nsu/ccfit/boltava/model/Server.java
@@ -1,57 +1,95 @@
 package ru.nsu.ccfit.boltava.model;
 
-import java.io.BufferedReader;
-import java.io.IOException;
-import java.io.InputStreamReader;
-import java.io.PrintWriter;
+import ru.nsu.ccfit.boltava.model.chat.ChatMember;
+import ru.nsu.ccfit.boltava.model.message.IMessageHandler;
+import ru.nsu.ccfit.boltava.model.message.ISocketMessageStream.MessageStreamType;
+import ru.nsu.ccfit.boltava.model.message.Message;
+
+import java.io.*;
 import java.net.ServerSocket;
 import java.net.Socket;
+import java.util.HashSet;
+import java.util.Properties;
+import java.util.concurrent.LinkedBlockingQueue;
 
 public class Server {
 
+    private HashSet<ChatMember> mChatMembers = new HashSet<>();
+    private LinkedBlockingQueue<Message> msgQueue = new LinkedBlockingQueue<>();
+    private IMessageHandler mMsgHandler;
+    private Thread mXMLListener;
+    private Thread mObjectListener;
+
+//    TODO
+//    create object socket listener
+//    create xml socket listener
+//
     public static void main(String[] args) {
-
         try {
-            ServerSocket server = new ServerSocket(1077);
-            Socket clientSocket = server.accept();
-
-            System.out.println("Client connected: " + clientSocket.getInetAddress());
-
-            PrintWriter out = new PrintWriter(clientSocket.getOutputStream());
-            BufferedReader in = new BufferedReader(new InputStreamReader(clientSocket.getInputStream()));
-            BufferedReader stdIn = new BufferedReader(new InputStreamReader(System.in));
-
-            Thread reader = new Thread(() -> {
-                String input;
-                try {
-                    while((input = in.readLine()) != null) {
-                        System.out.println("Incoming message: " + input);
-                    }
-                } catch (IOException e) {
-                    e.printStackTrace();
-                }
-            });
-            reader.start();
-
-            Thread writer = new Thread(() -> {
-                String input;
-                try {
-                    while((input = stdIn.readLine()) != null) {
-                        System.out.println("Outcoming message: " + input);
-                        out.println(input);
-                        out.flush();
-                    }
-                } catch (IOException e) {
-                    e.printStackTrace();
-                }
-            });
-            writer.start();
-
-            clientSocket.close();
-            server.close();
-
+            Server server = new Server();
+            server.createClientListeners();
+            server.start();
         } catch (IOException e) {
             e.printStackTrace();
+        } catch (NumberFormatException e) {
+            System.err.println("[Error] Invalid port number format: " + e.getMessage());
+        }
+
+
+    }
+
+    private void createClientListeners() throws IOException, NumberFormatException {
+        try (FileInputStream is = new FileInputStream("server.properties")) {
+            Server server = new Server();
+            Properties props = new Properties();
+            props.load(is);
+
+            mObjectListener = new Thread(new ClientsListener(Integer.parseInt(props.getProperty("object_port")),
+                    MessageStreamType.OBJ));
+            mXMLListener = new Thread(new ClientsListener(Integer.parseInt(props.getProperty("xml_port")),
+                    MessageStreamType.XML));
+        }
+    }
+
+    private void start() {
+        mXMLListener.start();
+        mObjectListener.start();
+    }
+
+    private void stop() {
+        mXMLListener.interrupt();
+        mObjectListener.interrupt();
+        // send SHUT_SOWN message to members?
+    }
+
+    private class ClientsListener implements Runnable {
+
+        private ServerSocket serverSocket;
+        private MessageStreamType streamType;
+
+        ClientsListener(int port, MessageStreamType type) throws IOException {
+            serverSocket = new ServerSocket(port);
+            streamType = type;
+        }
+
+        @Override
+        public void run() {
+            try {
+                while (!Thread.interrupted()) {
+                    Socket socket = serverSocket.accept();
+                    mChatMembers.add(new ChatMember(socket, mMsgHandler, streamType));
+
+                    System.out.println("Client connected: " + socket.getInetAddress());
+                }
+            } catch (IOException e) {
+                e.printStackTrace();
+            } finally {
+                try {
+                    serverSocket.close();
+                } catch (IOException e) {
+                    e.printStackTrace();
+                }
+            }
         }
 
     }
