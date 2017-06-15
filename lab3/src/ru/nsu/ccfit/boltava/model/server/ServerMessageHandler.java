67ede3e754e7004406fa6bd574f39def8a87ee25
Alexey Boltava <goodmoveab@gmail.com>
2017-06-15 20:33:41 +0700

    Improve requests handling
    Server can now handle more requests and respond with various error types

--- lab3/src/ru/nsu/ccfit/boltava/model/server/ServerMessageHandler.java
+++ lab3/src/ru/nsu/ccfit/boltava/model/server/ServerMessageHandler.java
@@ -5,9 +5,17 @@ import org.apache.logging.log4j.LogManager;
 import org.apache.logging.log4j.Logger;
 import ru.nsu.ccfit.boltava.model.chat.User;
 import ru.nsu.ccfit.boltava.model.message.*;
-import ru.nsu.ccfit.boltava.model.message.types.*;
+import ru.nsu.ccfit.boltava.model.message.notification.NewChatMessageNotification;
+import ru.nsu.ccfit.boltava.model.message.notification.UserJoinedChat;
+import ru.nsu.ccfit.boltava.model.message.request.GetUserList;
+import ru.nsu.ccfit.boltava.model.message.request.Login;
+import ru.nsu.ccfit.boltava.model.message.request.Logout;
+import ru.nsu.ccfit.boltava.model.message.request.SendChatMessage;
+import ru.nsu.ccfit.boltava.model.message.response.GetUserListSuccess;
+import ru.nsu.ccfit.boltava.model.message.response.LoginError;
+import ru.nsu.ccfit.boltava.model.message.response.LoginSuccess;
 
-import static ru.nsu.ccfit.boltava.model.server.Errors.ErrorName.*;
+import static ru.nsu.ccfit.boltava.model.server.ErrorBundle.ErrorName.*;
 
 public class ServerMessageHandler implements IServerMessageHandler {
 
@@ -23,65 +31,80 @@ public class ServerMessageHandler implements IServerMessageHandler {
 
     @Override
     public void handle(Login msg) throws InterruptedException {
-        logger.info(String.format("%s request. Username: %s", msg.getClass().getSimpleName(), msg.getUsername()));
+        logger.info(String.format("%s request. Username: %s", msg.getClass().getSimpleName(), msg.getSender()));
 
         Response response;
 
-        if (server.isUsernameTaken(msg.getUsername())) {
+        if (member.getSessionId() != null) {
             response = new LoginError(
-                    Errors.ErrorMessages.get(UsernameIsTaken),
-                    Errors.ErrorCodes.get(UsernameIsTaken)
+                    member.getSessionId(),
+                    ErrorBundle.ErrorMessages.get(DoubleLogin),
+                    ErrorBundle.ErrorCodes.get(DoubleLogin)
             );
-        } else if(!server.isUsernameFormatValid(msg.getUsername())) {
+        }
+        else if (server.isUsernameTaken(msg.getSender())) {
             response = new LoginError(
-                    Errors.ErrorMessages.get(InvalidUsernameFormat),
-                    Errors.ErrorCodes.get(InvalidUsernameFormat)
-                    // TODO
-                    // add expected msg format message
+                    member.getSessionId(),
+                    ErrorBundle.ErrorMessages.get(UsernameIsTaken),
+                    ErrorBundle.ErrorCodes.get(UsernameIsTaken)
             );
+        } else if(!server.isUsernameFormatValid(msg.getSender())) {
+            response = new LoginError(
+                    member.getSessionId(),
+                    ErrorBundle.ErrorMessages.get(InvalidUsernameFormat),
+                    ErrorBundle.ErrorCodes.get(InvalidUsernameFormat)
+            );
+            // TODO
+            // add "valid username format" message
         } else {
             String sessionId = String.valueOf(member.getID());
-            member.setUser(new User(msg.getUsername()));
-            server.registerUsername(msg.getUsername());
+            member.setUser(new User(msg.getSender()));
+            server.registerUsername(msg.getSender());
             member.setSessionId(sessionId);
-            response = new LoginSuccess(sessionId);
+            response = new LoginSuccess(sessionId, server.getChatHistorySneakPeek());
+
+            member.sendMessage(response);
+
+            Notification notification = new UserJoinedChat(
+                    member.getSessionId(),
+                    member.getUser()
+            );
+            server.enqueueMessage(notification);
+            return;
         }
 
         member.sendMessage(response);
-        // send message history to new user
-
-        Notification notification = new UpdateUserList(member.getUser(), UpdateUserList.Action.Add);
-        server.enqueueMessage(notification);
-
     }
 
     @Override
     public void handle(Logout msg) throws InterruptedException {
-        logger.info(String.format("%s request. Username: %s", msg.getClass().getSimpleName(), msg.getUsername()));
+        logger.info(String.format("%s request. Username: %s", msg.getClass().getSimpleName(), msg.getSender()));
 
     }
 
     @Override
-    public void handle(SendTextMessage msg) throws InterruptedException {
-        logger.info(String.format("%s request. Username: %s", msg.getClass().getSimpleName(), msg.getUsername()));
+    public void handle(SendChatMessage msg) throws InterruptedException {
+        logger.info(String.format("%s request. Username: %s", msg.getClass().getSimpleName(), msg.getSender()));
 
         ServerMessage serverMessage = checkMessageBroken(msg);
 
         if (serverMessage == null) {
-            serverMessage = new NewTextMessage(msg.getMessageText(), msg.getUsername());
+            serverMessage = new NewChatMessageNotification(member.getSessionId(), msg.getContent(), msg.getSender());
+            server.enqueueMessage(serverMessage);
+        } else {
+            member.sendMessage(serverMessage);
         }
 
-        member.sendMessage(serverMessage);
     }
 
     @Override
     public void handle(GetUserList msg) throws InterruptedException {
-        logger.info(String.format("%s request. Username: %s", msg.getClass().getSimpleName(), msg.getUsername()));
+        logger.info(String.format("%s request. Username: %s", msg.getClass().getSimpleName(), msg.getSender()));
 
         Response response = checkMessageBroken(msg);
 
         if (response == null) {
-            response = new GetUserListSuccess(server.getOnlineUsersList());
+            response = new GetUserListSuccess(member.getSessionId(), server.getOnlineUsersList());
         }
 
         member.sendMessage(response);
@@ -89,15 +112,38 @@ public class ServerMessageHandler implements IServerMessageHandler {
 
     private Response checkMessageBroken(Request msg) {
         Response response = null;
-        if (!msg.getUsername().equals(member.getUser().getUsername())){
+
+        if (member.getSessionId() == null) {
             response = new LoginError(
-                    Errors.ErrorMessages.get(UsernameMismatch),
-                    Errors.ErrorCodes.get(UsernameMismatch)
+                    member.getSessionId(),
+                    ErrorBundle.ErrorMessages.get(LoginRequired),
+                    ErrorBundle.ErrorCodes.get(LoginRequired)
             );
-        } else if (msg.getSessionId().equals(member.getSessionId())) {
+        }
+        else if (msg.getSender() == null) {
             response = new LoginError(
-                    Errors.ErrorMessages.get(SessionIdMismatch),
-                    Errors.ErrorCodes.get(SessionIdMismatch)
+                    member.getSessionId(),
+                    ErrorBundle.ErrorMessages.get(NullUsername),
+                    ErrorBundle.ErrorCodes.get(NullUsername)
+            );
+        } else if (msg.getSessionId() == null) {
+            response = new LoginError(
+                    member.getSessionId(),
+                    ErrorBundle.ErrorMessages.get(NullSessionId),
+                    ErrorBundle.ErrorCodes.get(NullSessionId)
+            );
+        }
+        else if (!member.getUser().getUsername().equals(msg.getSender())){
+            response = new LoginError(
+                    member.getSessionId(),
+                    ErrorBundle.ErrorMessages.get(UsernameMismatch),
+                    ErrorBundle.ErrorCodes.get(UsernameMismatch)
+            );
+        } else if (!member.getSessionId().equals(msg.getSessionId())) {
+            response = new LoginError(
+                    member.getSessionId(),
+                    ErrorBundle.ErrorMessages.get(SessionIdMismatch),
+                    ErrorBundle.ErrorCodes.get(SessionIdMismatch)
             );
         }
 
