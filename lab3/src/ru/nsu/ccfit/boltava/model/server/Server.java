00b3c2b68c430534d14838de351f1aa2f99d65ea
Alexey Boltava <goodmoveab@gmail.com>
2017-06-15 20:34:12 +0700

    Refactor Server class

--- lab3/src/ru/nsu/ccfit/boltava/model/server/Server.java
+++ lab3/src/ru/nsu/ccfit/boltava/model/server/Server.java
@@ -3,6 +3,7 @@ package ru.nsu.ccfit.boltava.model.server;
 import org.apache.logging.log4j.LogManager;
 import org.apache.logging.log4j.Logger;
 import ru.nsu.ccfit.boltava.model.message.ServerMessage;
+import ru.nsu.ccfit.boltava.model.message.message_content.ChatMessage;
 import ru.nsu.ccfit.boltava.model.net.ISocketMessageStream.MessageStreamType;
 
 import java.io.*;
@@ -16,8 +17,8 @@ public class Server {
 
     private static final Logger logger = LogManager.getLogger("ConsoleLogger");
 
-    private HashMap<Long, ChatMember> mChatMembers = new HashMap<>();
-    private HashSet<String> mReservesUserNames = new HashSet<>();
+    private HashSet<ChatMember> chatMembers = new HashSet<>();
+    private HashSet<String> reservedUserNames = new HashSet<>();
     private LinkedBlockingQueue<ServerMessage> mMsgQueue = new LinkedBlockingQueue<>();
 
     private Server server;
@@ -27,10 +28,11 @@ public class Server {
 
     private final String USERNAME_PATTERN_STRING = "(\\w+){5,}";
     private final Pattern usernamePattern = Pattern.compile(USERNAME_PATTERN_STRING);
+    private final int CHAT_HISTORY_SNEAK_PEEK_SIZE = 10;
 
-//    private List<TextMess>
-//    Add message history
+    private ArrayList<ChatMessage> chatHistory = new ArrayList<>();
 
+    private final Object lock = new Object();
 
     private Server() throws IOException {
         try (FileInputStream is = new FileInputStream("server.properties")) {
@@ -71,40 +73,57 @@ public class Server {
         // send SHUT_SOWN message to members?
     }
 
-    public void enqueueMessage(ServerMessage msg) throws InterruptedException {
+    void enqueueMessage(ServerMessage msg) throws InterruptedException {
         mMsgQueue.put(msg);
         logger.info("Put a message in queue: " + msg.getClass().getSimpleName());
     }
 
-    public boolean isUsernameTaken(String username) {
-        return mReservesUserNames.contains(username);
+    boolean isUsernameTaken(String username) {
+        return reservedUserNames.contains(username);
     }
 
-    public boolean isUsernameFormatValid(String username) {
+    boolean isUsernameFormatValid(String username) {
         return usernamePattern.matcher(username).matches();
     }
 
-    public void registerUsername(String username) {
-        mReservesUserNames.add(username);
+    void registerUsername(String username) {
+        synchronized (lock) {
+            reservedUserNames.add(username);
+        }
     }
 
-    private void broadcastMessage(MessageBox msgBox) throws InterruptedException {
-        for (ChatMember m : mChatMembers.values()) {
-            if (m.getID() != msgBox.senderID)
-                m.sendMessage(msgBox.message);
+    void removeChatMember(ChatMember member) {
+        synchronized (lock) {
+            reservedUserNames.remove(member.getUser().getUsername());
+            chatMembers.remove(member);
         }
     }
 
     private void broadcastMessage(ServerMessage msg) throws InterruptedException {
-        for (ChatMember m : mChatMembers.values()) {
-            if (!m.getSessionId().equals(msg.getSessionId())) {
+        for (ChatMember m : chatMembers) {
+            if (m.getSessionId() != null &&
+                !m.getSessionId().equals(msg.getSessionId())) {
                 m.sendMessage(msg);
             }
         }
     }
 
-    public List<String> getOnlineUsersList() {
-        return new ArrayList<>(mReservesUserNames);
+    ArrayList<ChatMessage> getChatHistorySneakPeek() {
+        ArrayList<ChatMessage> result = new ArrayList<>();
+        synchronized (lock) {
+            int historySize = chatHistory.size();
+            int min = CHAT_HISTORY_SNEAK_PEEK_SIZE < historySize ? CHAT_HISTORY_SNEAK_PEEK_SIZE : historySize;
+            for (int i = 0; i < min; i++) {
+                result.add(chatHistory.get(historySize - i - 1));
+            }
+            lock.notifyAll();
+        }
+
+        return result;
+    }
+
+    List<String> getOnlineUsersList() {
+        return new ArrayList<>(reservedUserNames);
     }
 
     private class ClientsListener implements Runnable {
@@ -123,7 +142,7 @@ public class Server {
                 while (!Thread.interrupted()) {
                     Socket socket = serverSocket.accept();
                     ChatMember member = new ChatMember(socket, server, streamType);
-                    mChatMembers.put(member.getID(), member);
+                    chatMembers.add(member);
                     logger.info("Client connected: " + socket.getInetAddress());
                 }
             } catch (IOException e) {
@@ -154,32 +173,6 @@ public class Server {
                 e.printStackTrace();
             }
 
-        }
-
-    }
-
-    static class MessageBox {
-        long senderID;
-        long receiverID;
-        ServerMessage message;
-        BroadcastCoverage coverage;
-
-        MessageBox() {}
-
-        MessageBox(long senderID,
-                   long receiverID,
-                   ServerMessage message,
-                   BroadcastCoverage coverage) {
-            this.senderID = senderID;
-            this.receiverID = receiverID;
-            this.message = message;
-            this.coverage = coverage;
-        }
-
-        public enum BroadcastCoverage {
-            Broadcast,
-            Multicast,
-            Unicast
         }
 
     }
