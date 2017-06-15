1d656656ef01c1d7aa6bf4f7cbcefb7c8a2d2714
Alexey Boltava <goodmoveab@gmail.com>
2017-06-15 20:30:57 +0700

    Better integration between chat member and server

--- lab3/src/ru/nsu/ccfit/boltava/model/server/ChatMember.java
+++ lab3/src/ru/nsu/ccfit/boltava/model/server/ChatMember.java
@@ -15,12 +15,15 @@ public class ChatMember {
     private long id = ID_GENERATOR.incrementAndGet();
     private User user;
     private String sessionId;
+
+    private Server server;
     private ServerMediator connection;
     private ServerMessageHandler messageHandler;
 
     public ChatMember(Socket socket,
                       Server server,
                       MessageStreamType type) throws IOException {
+        this.server = server;
         messageHandler = new ServerMessageHandler(server, this);
         connection = new ServerMediator(socket, messageHandler, type, this);
         connection.listen();
@@ -50,4 +53,24 @@ public class ChatMember {
         this.user = user;
     }
 
+    public void disconnect() {
+        server.removeChatMember(this);
+    }
+
+    @Override
+    public boolean equals(Object o) {
+        if (this == o) return true;
+        if (o == null || getClass() != o.getClass()) return false;
+
+        ChatMember that = (ChatMember) o;
+
+        return id == that.id && (sessionId != null ? sessionId.equals(that.sessionId) : that.sessionId == null);
+    }
+
+    @Override
+    public int hashCode() {
+        int result = (int) (id ^ (id >>> 32));
+        result = 31 * result + (sessionId != null ? sessionId.hashCode() : 0);
+        return result;
+    }
 }
