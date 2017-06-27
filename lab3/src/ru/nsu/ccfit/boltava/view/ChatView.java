5b3c2177204895c941a92afe0110cb5e5a482f81
Alexey Boltava <goodmoveab@gmail.com>
2017-06-27 23:47:01 +0700

    Fix cell height problems in message list

--- lab3/src/ru/nsu/ccfit/boltava/view/ChatView.java
+++ lab3/src/ru/nsu/ccfit/boltava/view/ChatView.java
@@ -6,6 +6,8 @@ import ru.nsu.ccfit.boltava.model.message.event.UserLeftChatEvent;
 
 import javax.swing.*;
 import java.awt.*;
+import java.awt.event.ComponentAdapter;
+import java.awt.event.ComponentEvent;
 
 
 public class ChatView extends JComponent implements IChatMessageRenderer {
@@ -17,70 +19,68 @@ public class ChatView extends JComponent implements IChatMessageRenderer {
     private JScrollPane scrollPane;
 
     ChatView() {
-        ListElementRenderer renderer = new ListElementRenderer();
+        ChatMessageRenderer renderer = new ChatMessageRenderer();
         messageList.setCellRenderer(renderer);
         messageList.setModel(model);
-        messageList.setVisible(true);
+        JViewport viewport = scrollPane.getViewport();
+        viewport.addChangeListener(e -> {
+            renderer.setWidth(viewport.getWidth()  - BORDER_OFFSET);
+            scrollPane.validate();
+            scrollPane.repaint();
+        });
+        messageList.addComponentListener(new ComponentAdapter() {
+            @Override
+            public void componentResized(ComponentEvent componentEvent) {
+                messageList.setFixedCellHeight(0);
+                messageList.setFixedCellHeight(-1);
+            }
+        });
+    }
+
+    private void renderMessage(String message) {
+        SwingUtilities.invokeLater(() -> {
+            model.addElement(message);
+            SwingUtilities.invokeLater(this::scrollToBottom);
+        });
+    }
+
+    private void scrollToBottom() {
+        int lastIndex = model.getSize() - 1;
+        if (lastIndex >= 0) {
+            messageList.ensureIndexIsVisible(lastIndex);
+        }
     }
 
     @Override
     public void render(TextMessage msg) {
-        SwingUtilities.invokeLater(
-                new Renderer(String.format(StylesBundle.TEXT_MESSAGE_MARKUP, msg.getAuthor(), msg.getMessage()))
-        );
+        renderMessage(String.format(StylesBundle.TEXT_MESSAGE_MARKUP, msg.getAuthor(), msg.getMessage()));
     }
 
     @Override
     public void render(UserLeftChatEvent msg) {
-        SwingUtilities.invokeLater(
-                new Renderer(String.format(StylesBundle.USERLIST_EVENT_MARKUP, msg.getUsername(), "left chat" ))
-        );
+        renderMessage(String.format(StylesBundle.USERLIST_EVENT_MARKUP, msg.getUsername(), "left chat" ));
     }
 
     @Override
     public void render(UserJoinedChatEvent msg) {
-        SwingUtilities.invokeLater(
-                new Renderer(String.format(StylesBundle.USERLIST_EVENT_MARKUP, msg.getUsername(), "joined chat" ))
-        );
-    }
-
-    private class Renderer implements Runnable {
-
-        String message;
-
-        Renderer(String message) {
-            this.message = message;
-        }
-
-        @Override
-        public void run() {
-            model.addElement(message);
-            SwingUtilities.invokeLater(this::scrollToBottom);
-        }
-
-        private void scrollToBottom() {
-            int lastIndex = model.getSize() - 1;
-            if (lastIndex >= 0) {
-                messageList.ensureIndexIsVisible(lastIndex);
-            }
-        }
-
+        renderMessage(String.format(StylesBundle.USERLIST_EVENT_MARKUP, msg.getUsername(), "joined chat" ));
     }
 
 
-    private class ListElementRenderer extends DefaultListCellRenderer {
+    private class ChatMessageRenderer extends DefaultListCellRenderer {
 
         private int width = super.getWidth();
 
         private static final String CHAT_MESSAGE_FORMAT =
                 "<html>" +
-                    "<body><p WIDTH=%d style='padding:5px'>%s</p</body>" +
+					"<p WIDTH=%d style='padding:5px'>" +
+                            "%s" +
+                    "</p>" +
                 "</html>";
 
         @Override
         public Component getListCellRendererComponent(JList list, Object object,
                                                       int index, boolean isSelected, boolean hasFocus) {
-            width = scrollPane.getViewport().getWidth()  - BORDER_OFFSET;
             final String text = String.format(CHAT_MESSAGE_FORMAT, width, object.toString());
             return super.getListCellRendererComponent(list, text, index, isSelected, hasFocus);
         }
@@ -94,8 +94,10 @@ public class ChatView extends JComponent implements IChatMessageRenderer {
     private static class StylesBundle {
 
         static final String TEXT_MESSAGE_MARKUP = (
+//                "<p>" +
                     "<span style='color:blue;'>%s</span><br>" +
                     "<span style='font-size:1.1em;'>%s</span>"
+//                "</p>"
         );
 
         static final String USERLIST_EVENT_MARKUP = (
